9e32cb1403cb6fadb7d6366e4c01c1af0b3cc5cd
Can <can.koyuncu>
2025-02-24 13:59:22 +0300

    Initial Commit

---
+++ AnnouncementCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnnouncementCell
+//
+//  Created by can.koyuncu on 24.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
