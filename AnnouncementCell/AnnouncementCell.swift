de2db41a35f5ae4471c1d1c9cebcaf65b3353b45
Can <can.koyuncu>
2025-02-25 13:29:21 +0300

    test

---
+++ AnnouncementCell/AnnouncementCell.swift
@@ -0,0 +1,8 @@
+//
+//  AnnouncementCell.swift
+//  AnnouncementCell
+//
+//  Created by can.koyuncu on 25.02.2025.
+//
+
+import Foundation
---
+++ AnnouncementCell/CustomBannerView.swift
@@ -0,0 +1,8 @@
+//
+//  CustomBannerView.swift
+//  AnnouncementCell
+//
+//  Created by can.koyuncu on 24.02.2025.
+//
+
+import Foundation
